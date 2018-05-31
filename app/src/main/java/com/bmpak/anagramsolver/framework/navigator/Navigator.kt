14133768c958bffb632f4a7b43eddb8463639b7c
charbgr <bmpak04@gmail.com>
2018-05-31 17:16:03 +0300

    Introduce Navigator

---
+++ app/src/main/java/com/bmpak/anagramsolver/framework/navigator/Navigator.kt
@@ -0,0 +1,6 @@
+package com.bmpak.anagramsolver.framework.navigator
+
+interface Navigator {
+  fun toOnboarding()
+  fun toMainScreen()
+}
---
+++ app/src/main/java/com/bmpak/anagramsolver/framework/navigator/RealNavigator.kt
@@ -0,0 +1,28 @@
+package com.bmpak.anagramsolver.framework.navigator
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import com.bmpak.anagramsolver.MainActivity
+import com.bmpak.anagramsolver.ui.onboarding.OnboardingScreen
+
+class RealNavigator(
+    private val context: Context
+) : Navigator {
+
+  override fun toOnboarding() {
+    val intent = createIntent(OnboardingScreen::class.java)
+    context.startActivity(intent)
+  }
+
+  override fun toMainScreen() {
+    val intent = createIntent(MainActivity::class.java)
+    context.startActivity(intent)
+  }
+
+  private fun createIntent(klazz: Class<*>): Intent = Intent(context, klazz).apply {
+    if (context !is Activity) {
+      this.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    }
+  }
+}
