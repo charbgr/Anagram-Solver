1a6d03834213fe550db567381519349c134df034
charbgr <bmpak04@gmail.com>
2018-05-31 17:15:54 +0300

    Scaffold OnboardingScreen

---
+++ app/src/main/java/com/bmpak/anagramsolver/ui/onboarding/OnboardingScreen.kt
@@ -0,0 +1,14 @@
+package com.bmpak.anagramsolver.ui.onboarding
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bmpak.anagramsolver.R
+
+class OnboardingScreen : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_onboarding)
+  }
+
+}
