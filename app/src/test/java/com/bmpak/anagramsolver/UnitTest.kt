6be33a16db72e21854075f8090435399d473cfc7
charbgr <bmpak04@gmail.com>
2019-02-13 22:12:46 +0200

    Replace CoroutineContextProvider with SchedulerProvider

--- app/src/test/java/com/bmpak/anagramsolver/UnitTest.kt
+++ app/src/test/java/com/bmpak/anagramsolver/UnitTest.kt
@@ -1,16 +1,17 @@
 package com.bmpak.anagramsolver
 
-import com.bmpak.anagramsolver.framework.arch.CoroutineContextProvider
-import kotlinx.coroutines.experimental.Unconfined
+import com.bmpak.anagramsolver.framework.arch.SchedulerProvider
+import io.reactivex.Scheduler
+import io.reactivex.schedulers.Schedulers
 import timber.log.Timber
-import kotlin.coroutines.experimental.CoroutineContext
 
 open class UnitTest {
   companion object {
-    val NOW = Unconfined
-    val NOW_CO_PROVIDER = object : CoroutineContextProvider {
-      override val asyncPool: CoroutineContext = Unconfined
-      override val ui: CoroutineContext = Unconfined
+    val NOW_SCHEDULER = Schedulers.trampoline()
+    val NOW_SCHEDULER_PROVIDER = object : SchedulerProvider {
+      override val io: Scheduler = NOW_SCHEDULER
+      override val ui: Scheduler = NOW_SCHEDULER
+      override val computation: Scheduler = NOW_SCHEDULER
     }
   }
 
