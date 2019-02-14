05e95cb7c138d519495f1e02dcb500912e9f3fc7
charbgr <bmpak04@gmail.com>
2019-02-14 18:24:57 +0200

    Transform usecases to Rx

--- app/src/main/java/com/bmpak/anagramsolver/framework/usecase/FetchWorker.kt
+++ app/src/main/java/com/bmpak/anagramsolver/framework/usecase/FetchWorker.kt
@@ -3,29 +3,16 @@ package com.bmpak.anagramsolver.framework.usecase
 import androidx.work.Worker
 import com.bmpak.anagramsolver.framework.repository.dictionary.FirebaseDictionaryRepository
 import com.bmpak.anagramsolver.model.Dictionary
-import com.bmpak.anagramsolver.utils.Either.Left
-import com.bmpak.anagramsolver.utils.Either.Right
-import kotlinx.coroutines.experimental.runBlocking
 import timber.log.Timber
 
 class FetchWorker : Worker() {
   override fun doWork(): Worker.Result {
-    Timber.d("Work : Start")
-    val either = runBlocking {
-      FetchDictionaryUseCase(FirebaseDictionaryRepository).run(Dictionary.GREEK)
+    Timber.d("FetchWorker : Start")
+    val flowable = FetchDictionaryUseCase(FirebaseDictionaryRepository).build(Dictionary.GREEK)
+    flowable.blockingForEach { downloadStatus ->
+      Timber.d("DownloadStatus : $downloadStatus")
     }
-    if (either is Right) {
-      return Worker.Result.FAILURE
-    }
-    either as Left
-
-    runBlocking {
-      for (item in either.value) {
-        Timber.d("Work : $item")
-      }
-    }
-
-    Timber.d("Work : Return")
+    Timber.d("FetchWorker : Return")
     return Worker.Result.SUCCESS
   }
 }
--- app/src/main/java/com/bmpak/anagramsolver/framework/usecase/SearchUseCase.kt
+++ app/src/main/java/com/bmpak/anagramsolver/framework/usecase/SearchUseCase.kt
@@ -1,19 +1,21 @@
 package com.bmpak.anagramsolver.framework.usecase
 
-import com.bmpak.anagramsolver.framework.arch.CoroutineContextProvider
+import com.bmpak.anagramsolver.framework.arch.SchedulerProvider
+import com.bmpak.anagramsolver.framework.arch.SingleUseCase
 import com.bmpak.anagramsolver.framework.repository.anagram.AnagramRepository
-import com.bmpak.anagramsolver.utils.Either
+import io.reactivex.Single
 
 class SearchUseCase(
-    private val repository: AnagramRepository,
-    coContextProvider: CoroutineContextProvider = CoroutineContextProvider.Real
-) : UseCase<List<String>, CharSequence>(coContextProvider) {
+  private val repository: AnagramRepository,
+  schedulerProvider: SchedulerProvider = SchedulerProvider.Real
+) : SingleUseCase<List<String>, CharSequence>(schedulerProvider) {
 
   @Suppress("PARAMETER_NAME_CHANGED_ON_OVERRIDE")
-  override suspend fun run(query: CharSequence): Either<List<String>, Throwable> {
+  override fun build(query: CharSequence): Single<List<String>> {
     val queryChars = query.toString().toCharArray()
     val safeQuery = queryChars.sortedArray().joinToString("")
     return repository.fetch(safeQuery)
+      .subscribeOn(schedulerProvider.io)
+      .observeOn(schedulerProvider.ui)
   }
-
 }
