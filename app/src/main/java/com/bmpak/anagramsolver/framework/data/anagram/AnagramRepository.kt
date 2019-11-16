f851a28f6ab3e4ed814f3173cd5f567212418a9f
charbgr <bmpak04@gmail.com>
2019-11-16 12:28:26 +0200

    Add private modifiers to anagram repository

--- app/src/main/java/com/bmpak/anagramsolver/framework/data/anagram/AnagramRepository.kt
+++ app/src/main/java/com/bmpak/anagramsolver/framework/data/anagram/AnagramRepository.kt
@@ -4,8 +4,8 @@ import com.bmpak.anagramsolver.model.Anagram
 import io.reactivex.Single
 
 class AnagramRepository(
-    val dataSource: AnagramDataSource,
-    val mapper: AnagramEntityMapper
+    private val dataSource: AnagramDataSource,
+    private val mapper: AnagramEntityMapper
 ) {
 
   fun fetchAnagrams(query: CharSequence): Single<List<Anagram>> =
